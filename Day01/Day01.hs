2415042b255892151c3aa25db3bf19dbb1ae1807
jrp2014 <jrp2014@users.noreply.github.com>
2020-12-01 19:40:55 +0000

    Day 1, small optimization

--- Day01/Day01.hs
+++ Day01/Day01.hs
@@ -1,21 +1,29 @@
 module Day01 where
 
-import Data.List (tails)
+import Data.List ( tails )
 
-ex1 :: [Int]
-ex1 = [1721, 979, 366, 299, 675, 1456]
+ex1 :: [ Int ]
+ex1 = [ 1721, 979, 366, 299, 675, 1456 ]
 
-parse :: String -> [Int]
+parse :: String -> [ Int ]
 parse s = read <$> lines s
 
-part1 :: [Int] -> Int
-part1 expenses = head [x * y | x : xs <- tails expenses, y <- xs, x + y == 2020]
+part1 :: [ Int ] -> Int
+part1 expenses = head
+    [ x * y | x : xs <- tails expenses, x < 2020, y <- xs, x + y == 2020 ]
 
-part2 :: [Int] -> Int
-part2 expenses = head [x * y * z | x : xs <- tails expenses, y : ys <- tails xs, z <- ys, x + y + z == 2020]
+part2 :: [ Int ] -> Int
+part2 expenses = head
+    [ x * y * z | x : xs <- tails expenses
+                , x < 2020
+                , y : ys <- tails xs
+                , y < 2020
+                , z <- ys
+                , x + y + z == 2020
+    ]
 
 main :: IO ()
 main = do
-  input <- readFile "input.txt"
-  print $ part1 (parse input)
-  print $ part2 (parse input)
+    input <- readFile "input.txt"
+    print $ part1 (parse input)
+    print $ part2 (parse input)
