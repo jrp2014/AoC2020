ca321e0af19acf15677880b4f25bc95ff4ba9404
jrp2014 <jrp2014@users.noreply.github.com>
2020-12-25 01:17:56 +0000

    Additional approach to Day 3

--- Day10/Day10.hs
+++ Day10/Day10.hs
@@ -13,10 +13,25 @@ part1 = product . map length . group . sort . diffs
 
 -- https://topaz.github.io/paste/#XQAAAQBjBAAAAAAAAAA2m8ixrhLu7YJFrd2FLde+PAG1Aui2yN36LC93WIQ2APMCiCS0L/ERCZO1Ub196UifbJLLVlbSvYxOwAjYDrQYBebXdg1tlZnkpCddIzEyACJhqVaVyfEHliHhHTo43RuH6MUu0/w7UwQVDRxlqCPYNFIpYzS1qKJg/dS0LL2ErMa11sDhjnpt6/o51Ih9QtTRsUzO6GnqZLny1iBhMg0nSw2pDcXMryFYJwvLEhhnOA2VNJhWM1Gn+OCdev9TOOzuOQowvMP36UPzxmVWAUAZjDS15RcLbFFeojz/Q3BXsrrnJMBiNxhpkR4YrDjCAuCJQhksI2fE4chaAJFvJaPw5UlpCtQuiVbbqc3yXAqMc7ZCzCRvX+0srUpCaftCCjjva+MKRJRQzBKSunnKeBkSDwczfvMFeCOMUAgJNzevKG/9iDc4NV+n56zO45uDz83yZF37h0INKpfAtZ1zhvULILedD6BLrh+7SiN9L/heBGFZY/MeDx08C7N1dnUipMqmyHZYt3OLuS/tMCidJlWOzC47ZbSiX3wpWhz5pYzuaeAAOqt+/Rvbsm8RPpwnCrilLLtPJxBszSf/L+y/EDDgy6BfAaMq+4xMSOts+1hnxNL2wkja2D5aDr94d7rWSrcLEvswpKdPekGRmAJ9ph5eo4Eof0NKgZqb7BZ8WjzkDxcRvsq0aPFYPzt7XrE0PcKuY2DIHRC6dJvlmMk3QK3n9jjwd6jRN1F9n0ggIjvf5dh9uRrJ3uPhP6jwta+yGXyYZCnlkS59jZZLNJ9M8WPtctV6p2PKoZWKVLpONniR9BG3kjjVnL+SSlDSKZT1fkyu0MUzSNUiiu6H/vtZjJ4=
 part2 :: [Int] -> Int
-part2 = product . map ((dp !!) . length) . filter ((== 1) . head) . group . diffs
+part2 =
+  product . map ((dp !!) . length) . filter ((== 1) . head) . group
+    . diffs
   where
     -- dp[i] represents the number of arrangements which end at adapter i.
     dp = 1 : 1 : 2 : zipWith3 (\x y z -> x + y + z) dp (drop 1 dp) (drop 2 dp)
+
+part2' :: [Int] -> Int
+part2' xs = last dp
+  where
+    xs' = sort xs :: [Int]
+
+    xs'' = 0 : xs' ++ [last xs' + 3] :: [Int]
+
+    dp = 1 : (dp' <$> tail xs'') :: [Int]
+
+    dp' :: Int -> Int
+    dp' x =
+      sum $ map snd . takeWhile ((< x) . fst) . dropWhile ((> 3) . (-) x . fst) $ zip xs'' dp
 
 main :: IO ()
 main = do
@@ -24,6 +39,7 @@ main = do
   let pinput = makeList input
   print $ part1 pinput
   print $ part2 pinput
+  print $ part2' input
 
 ex1 :: [Int]
 ex1 =
